942fc3862926e06a828e9451596af23f627df19a
Natalia <natalia.liscio@gmail.com>
2023-11-22 15:06:20 -0300

    Actualización

--- Laboratorio Unidad 3/Ejercicio15.py
+++ Laboratorio Unidad 3/Ejercicio15.py
@@ -1,5 +1,11 @@
 """
-
-
-
+Ejercicio 15
+Ingresar el sueldo de una persona, si supera los 3000 dolares mostrar un mensaje en pantalla indicando que debe abonar impuestos.
 """
+sueldo = float(input("Ingrese el sueldo de la persona: "))
+if sueldo > 3000:
+    print("Debe abonar impuestos")
+else:   
+    print("No debe abonar impuestos")
+    
+    
